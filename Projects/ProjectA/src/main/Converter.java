583b06558f7118f5b48097269015489a52c48d17
jrl1024 <jrl1024@wildcats.unh.edu>
2021-07-15 16:09:21 -0400

    Project A: moved secondary menue to seperate methods to clean up code

--- Projects/ProjectA/src/main/Converter.java
+++ Projects/ProjectA/src/main/Converter.java
@@ -70,13 +70,97 @@ public class Converter {
 		return gallon;
 	}
 	
+	public boolean goVolume(Scanner input)
+	{
+		
+		helpVolume();
+		String choice = input.nextLine().toLowerCase();
+		System.out.println(choice);
+		if (choice.equals("back") || choice.equals("b") || choice.equals("4"))
+		{
+			//do nothing, cancel other ifs
+		}
+		else if (choice.equals("help") || choice.equals("h") || choice.equals("6"))
+		{
+			
+			//help option
+			return goVolume(input);
+		}
+		if (choice.equals("quit") || choice.equals("q") || choice.equals("5"))
+		{
+			System.out.println("You have quit the program");
+			return false;
+		}
+		else if (choice.equals("teaspoons to tablespoons") || choice.equals("1"))
+		{
+			System.out.println("How many Teaspoons?");
+			double measurement = Double.parseDouble(input.nextLine());
+			double table = teaspoonsToTablespoons(measurement);
+			System.out.println(measurement + " Teaspoons equals " + table + " Tablespoons");
+		}
+		else if (choice.equals("cups to teaspoons") || choice.equals("2"))
+		{
+			System.out.println("How many Cups?");
+			double measurement = Double.parseDouble(input.nextLine());
+			double tea = cupsToTeaspoons(measurement);
+			System.out.println(measurement + " Cups equals " + tea + " Teaspoons");
+		}
+		else if (choice.equals("us gallons to imperial gallons") || choice.equals("3"))
+		{
+			System.out.println("How many US Gallons?");
+			double measurement = Double.parseDouble(input.nextLine());
+			double gallon = usToImperial(measurement);
+			System.out.println(measurement + " US Gallons equals " + gallon + " Imperial Gallons");
+		}
+		return true;
+	}
+	
+	public boolean goDistance(Scanner input)
+	{
+		
+		helpDistance();
+		String choice = input.nextLine().toLowerCase();
+		System.out.println(choice);
+		if (choice.equals("back") || choice.equals("b") || choice.equals("3"))
+		{
+			//do nothing, cancel other ifs
+		}
+		else if (choice.equals("help") || choice.equals("h") || choice.equals("5"))
+		{
+			
+			//help option
+			return goDistance(input);
+		}
+		if (choice.equals("quit") || choice.equals("q") || choice.equals("4"))
+		{
+			System.out.println("You have quit the program");
+			return false;
+		}
+		else if (choice.equals("feet to meters") || choice.equals("1"))
+		{
+			System.out.println("How many Feet?");
+			double measurement = Double.parseDouble(input.nextLine());
+			double meter = feetToMeters(measurement);
+			System.out.println(measurement + " Feet equals " + meter + " Meters");
+		}
+		else if (choice.equals("Miles to Kilometers") || choice.equals("2"))
+		{
+			System.out.println("How many Miles?");
+			double measurement = Double.parseDouble(input.nextLine());
+			double kilo = milesToKilometers(measurement);
+			System.out.println(measurement + " Miles equals " + kilo + " Kilometers");
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		
 		Converter con = new Converter();
 		
 		
 		Scanner input = new Scanner(System.in);
-		while (true) {
+		boolean cont = true;
+		while (cont) {
 			con.helpMain();
 			String choice = input.nextLine().toLowerCase();
 			//System.out.println(choice);
@@ -93,84 +177,14 @@ public class Converter {
 			else if (choice.equals("volume conversion") || choice.equals("v") || choice.equals("1"))
 			{
 				//enter volume conversion
-				con.helpVolume();
-				choice = input.nextLine().toLowerCase();
-				System.out.println(choice);
-				if (choice.equals("back") || choice.equals("b") || choice.equals("4"))
-				{
-					//do nothing, cancel other ifs
-				}
-				else if (choice.equals("help") || choice.equals("h") || choice.equals("6"))
-				{
-					
-					//help option
-					con.helpVolume();
-					choice = input.nextLine().toLowerCase();
-				}
-				if (choice.equals("quit") || choice.equals("q") || choice.equals("5"))
-				{
-					System.out.println("You have quit the program");
-					break;
-				}
-				else if (choice.equals("teaspoons to tablespoons") || choice.equals("1"))
-				{
-					System.out.println("How many Teaspoons?");
-					double measurement = Double.parseDouble(input.nextLine());
-					double table = con.teaspoonsToTablespoons(measurement);
-					System.out.println(measurement + " Teaspoons equals " + table + " Tablespoons");
-				}
-				else if (choice.equals("cups to teaspoons") || choice.equals("2"))
-				{
-					System.out.println("How many Cups?");
-					double measurement = Double.parseDouble(input.nextLine());
-					double tea = con.cupsToTeaspoons(measurement);
-					System.out.println(measurement + " Cups equals " + tea + " Teaspoons");
-				}
-				else if (choice.equals("us gallons to imperial gallons") || choice.equals("3"))
-				{
-					System.out.println("How many US Gallons?");
-					double measurement = Double.parseDouble(input.nextLine());
-					double gallon = con.usToImperial(measurement);
-					System.out.println(measurement + " US Gallons equals " + gallon + " Imperial Gallons");
-				}
+				cont = con.goVolume(input);
+				
 			}
 			else if (choice.equals("distance conversion") || choice.equals("d") || choice.equals("2"))
 			{
 				
 				//enter distance conversion
-				con.helpDistance();
-				choice = input.nextLine().toLowerCase();
-				System.out.println(choice);
-				if (choice.equals("back") || choice.equals("b") || choice.equals("3"))
-				{
-					//do nothing, cancel other ifs
-				}
-				else if (choice.equals("help") || choice.equals("h") || choice.equals("5"))
-				{
-					
-					//help option
-					con.helpDistance();
-					choice = input.nextLine().toLowerCase();
-				}
-				if (choice.equals("quit") || choice.equals("q") || choice.equals("4"))
-				{
-					System.out.println("You have quit the program");
-					break;
-				}
-				else if (choice.equals("feet to meters") || choice.equals("1"))
-				{
-					System.out.println("How many Feet?");
-					double measurement = Double.parseDouble(input.nextLine());
-					double meter = con.feetToMeters(measurement);
-					System.out.println(measurement + " Feet equals " + meter + " Meters");
-				}
-				else if (choice.equals("Miles to Kilometers") || choice.equals("2"))
-				{
-					System.out.println("How many Miles?");
-					double measurement = Double.parseDouble(input.nextLine());
-					double kilo = con.milesToKilometers(measurement);
-					System.out.println(measurement + " Miles equals " + kilo + " Kilometers");
-				}
+				cont = con.goDistance(input);
 			}
 			
 		}
