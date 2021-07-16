b71c8194db3e76cb57452c82dd7447d4c2a3a623
jrl1024 <jrl1024@wildcats.unh.edu>
2021-07-16 15:50:20 -0400

    Project A: added more options for volume and distance conversions

--- Projects/ProjectA/src/main/Converter.java
+++ Projects/ProjectA/src/main/Converter.java
@@ -17,20 +17,27 @@ public class Converter {
 		System.out.println("1. Teaspoons to Tablespoons");
 		System.out.println("2. Cups to Teaspoons");
 		System.out.println("3. US Gallons to Imperial Gallons");
-		System.out.println("4. Back to Conversion Type Menue");
-		System.out.println("5. Quit");
-		System.out.println("6. Help");
+		System.out.println("4. Liter to US Fluid Ounce");
+		System.out.println("5. Imperial Pint to Milliliter");
+		System.out.println("6. Back to Conversion Type Menue");
+		System.out.println("7. Quit");
+		System.out.println("8. Help");
 	}
 	
 	public void helpDistance() {
 		
 		System.out.println("1. Feet to Meters");
 		System.out.println("2. Miles to Kilometers");
-		System.out.println("3. Back to Conversion Type Menue");
-		System.out.println("4. Quit");
-		System.out.println("5. Help");
+		System.out.println("3.Centimeters to Inches");
+		System.out.println("4. Inches to Meters");
+		System.out.println("5. Kilometers to Yards");
+		System.out.println("6. Back to Conversion Type Menue");
+		System.out.println("7. Quit");
+		System.out.println("8. Help");
 	}
 	
+	
+	//Volume Conversions
 	public double teaspoonsToTablespoons(double num)
 	{
 		
@@ -46,6 +53,31 @@ public class Converter {
 		return tea;
 	}
 	
+	public double usToImperial(double num)
+	{
+		
+		//1 us gallon = 0.832674 imperial gallons
+		double gallon = num * 0.832674;
+		return gallon;
+	}
+	
+	public double litersToOunces(double num)
+	{
+		
+		//1 liter = 33.814 US Fluid Ounce
+		double ounce = num * 33.814;
+		return ounce;
+	}
+	
+	public double pintsToMilliliters(double num)
+	{
+		
+		//1 Imperial Pint = 568.2608686967 Milliliter
+		double mil = num * 568.2608686967;
+		return mil;
+	}
+	
+	//Distance Conversions
 	public double feetToMeters(double num)
 	{
 		
@@ -62,12 +94,28 @@ public class Converter {
 		return kilo;
 	}
 	
-	public double usToImperial(double num)
+	public double centimetersToInches(double num)
 	{
 		
-		//1 us gallon = 0.832674 imperial gallons
-		double gallon = num * 0.832674;
-		return gallon;
+		//1 centimeter = 0.393701 inches
+		double inch = num * 0.393701;
+		return inch;
+	}
+	
+	public double inchesToMeters(double num)
+	{
+		
+		//1 inch = 0.025400013716 meters
+		double meter = num * 0.025400013716;
+		return meter;
+	}
+	
+	public double kilometersToYards(double num)
+	{
+		
+		//1 kilometer = 1093.613888889 yards
+		double yard = num * 1093.613888889;
+		return yard;
 	}
 	
 	public boolean goVolume(Scanner input)
@@ -76,17 +124,17 @@ public class Converter {
 		helpVolume();
 		String choice = input.nextLine().toLowerCase();
 		System.out.println(choice);
-		if (choice.equals("back") || choice.equals("b") || choice.equals("4"))
+		if (choice.equals("back") || choice.equals("b") || choice.equals("6"))
 		{
 			//do nothing, cancel other ifs
 		}
-		else if (choice.equals("help") || choice.equals("h") || choice.equals("6"))
+		else if (choice.equals("help") || choice.equals("h") || choice.equals("8"))
 		{
 			
 			//help option
 			return goVolume(input);
 		}
-		if (choice.equals("quit") || choice.equals("q") || choice.equals("5"))
+		if (choice.equals("quit") || choice.equals("q") || choice.equals("7"))
 		{
 			System.out.println("You have quit the program");
 			return false;
@@ -112,6 +160,20 @@ public class Converter {
 			double gallon = usToImperial(measurement);
 			System.out.println(measurement + " US Gallons equals " + gallon + " Imperial Gallons");
 		}
+		else if (choice.equals("liter to us fluid ounce") || choice.equals("4"))
+		{
+			System.out.println("How many Liters?");
+			double measurement = Double.parseDouble(input.nextLine());
+			double ounce = litersToOunces(measurement);
+			System.out.println(measurement + " Liters equals " + ounce + " US Fluid Ounces");
+		}
+		else if (choice.equals("imperial pint to milliliter") || choice.equals("5"))
+		{
+			System.out.println("How many Imperial Pints?");
+			double measurement = Double.parseDouble(input.nextLine());
+			double mil = pintsToMilliliters(measurement);
+			System.out.println(measurement + " Imperial Pints equals " + mil + " Milliliters");
+		}
 		return true;
 	}
 	
@@ -121,17 +183,17 @@ public class Converter {
 		helpDistance();
 		String choice = input.nextLine().toLowerCase();
 		System.out.println(choice);
-		if (choice.equals("back") || choice.equals("b") || choice.equals("3"))
+		if (choice.equals("back") || choice.equals("b") || choice.equals("6"))
 		{
 			//do nothing, cancel other ifs
 		}
-		else if (choice.equals("help") || choice.equals("h") || choice.equals("5"))
+		else if (choice.equals("help") || choice.equals("h") || choice.equals("8"))
 		{
 			
 			//help option
 			return goDistance(input);
 		}
-		if (choice.equals("quit") || choice.equals("q") || choice.equals("4"))
+		if (choice.equals("quit") || choice.equals("q") || choice.equals("7"))
 		{
 			System.out.println("You have quit the program");
 			return false;
@@ -143,12 +205,33 @@ public class Converter {
 			double meter = feetToMeters(measurement);
 			System.out.println(measurement + " Feet equals " + meter + " Meters");
 		}
-		else if (choice.equals("Miles to Kilometers") || choice.equals("2"))
+		else if (choice.equals("miles to kilometers") || choice.equals("2"))
 		{
 			System.out.println("How many Miles?");
 			double measurement = Double.parseDouble(input.nextLine());
 			double kilo = milesToKilometers(measurement);
 			System.out.println(measurement + " Miles equals " + kilo + " Kilometers");
+		}
+		else if (choice.equals("centimeters to inches") || choice.equals("3"))
+		{
+			System.out.println("How many Centimeters?");
+			double measurement = Double.parseDouble(input.nextLine());
+			double inch = centimetersToInches(measurement);
+			System.out.println(measurement + " Centimeters equals " + inch + " Inches");
+		}
+		else if (choice.equals("inches to meters") || choice.equals("4"))
+		{
+			System.out.println("How many Inches?");
+			double measurement = Double.parseDouble(input.nextLine());
+			double meter = inchesToMeters(measurement);
+			System.out.println(measurement + " Inches equals " + meter + " Meters");
+		}
+		else if (choice.equals("kilometers to yards") || choice.equals("5"))
+		{
+			System.out.println("How many Kilometers?");
+			double measurement = Double.parseDouble(input.nextLine());
+			double yard = kilometersToYards(measurement);
+			System.out.println(measurement + " Kilmeters equals " + yard + " Yards");
 		}
 		return true;
 	}
